05dd596425e2f45edccbe04fd366d2e501a53242
kimkyungsub <cyon256@icloud.com>
2023-07-16 21:06:08 +0900

    webdev

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Eric-lab-star/webDev
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me<a href=\"mailto:cyon256@icloud.com\"> cyon256@icloud.com</a>")
+}
+
+func page404(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+	fmt.Fprint(w, "<h1>Page Not Found</h1>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, r.URL.Path)
+	fmt.Fprintln(w, r.URL.RawPath)
+	// switch r.URL.Path {
+	// case "/":
+	// 	homeHandler(w, r)
+	// case "/contact":
+	// 	contactHandler(w, r)
+	// default:
+	// 	page404(w, r)
+	// }
+}
+
+func main() {
+	http.HandleFunc("/", pathHandler)
+	fmt.Println("Server is running ")
+	http.ListenAndServe("localhost:3000", nil)
+}
